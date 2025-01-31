4cb89d72099864a0c9e421a57e9066b16c1745e5
Walter Moreira <walter@waltermoreira.net>
2025-01-30 20:21:29 -0600

    Simple example of definition

--- CommutativeAlgebra/Basic.lean
+++ CommutativeAlgebra/Basic.lean
@@ -1,2 +1,10 @@
 import Mathlib
 
+variable {R : Type*} [Ring R] {I : Ideal R}
+
+open Polynomial
+
+def homogeneous (I : Ideal R) (p : R[X]) : Prop :=
+  ∀ i : ℕ, i < p.degree → p.coeff i ∈ I ^ (p.natDegree - i)
+
+def isIntegralElem (x : R) := ∃ p : R[X], Monic p ∧ homogeneous I p ∧ eval x p = 0
